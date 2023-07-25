dd5e1aa28d5d9c4ec46a19901670925551b9b48b
kamala kannan <kamalroosewelt@gmail.com>
2023-07-25 22:22:30 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,9 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
+from sklearn.metrics import r2_score
+#from sklearn.model_selection import GridSearchCV
 #from sklearn.metrics import r2_score
 #from sklearn.model_selection import GridSearchCV
 
@@ -23,4 +26,37 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
-        
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report= {}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+
+            model.fit(X_train,y_train) #train_model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score= r2_score(y_train,y_train_pred)
+            test_mdoel_score= r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_mdoel_score
+
+        return report
+    
+    except  Exception as e:
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+    
+    
