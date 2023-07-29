6c2513172ed9851ebb6228fb719b42c0f8ac527d
kamala kannan <kamalroosewelt@gmail.com>
2023-07-29 16:38:57 +0530

    localdeploy

--- src/utils.py
+++ src/utils.py
@@ -3,16 +3,12 @@
 #find my best_model,train_test_split are genric function
 import os
 import sys
-
 import numpy as np
 import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
-#from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -27,13 +23,18 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report= {}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs= GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train) #train_model
 
             y_train_pred = model.predict(X_train)
